e901aab05dbf27faac9416294ae185984b9af7c0
oscarlehuu <oscar.lehuu@gmail.com>
2024-02-24 18:15:42 +1030

    [init project]

---
+++ ServerApp/Model/DatabaseChangelog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Booking_Systems;
+
+public class DatabaseChangelog : DbContext
+{
+  public DatabaseChangelog(DbContextOptions<DatabaseChangelog> options) : base(options) {
+
+  }
+  public DbSet<Organization> Organizations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new OrganizationConfiguration());
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,43 @@
+using Booking_Systems;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseChangelog>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope()) 
+{
+    var services = scope.ServiceProvider;
+    try 
+    {
+        var db = scope.ServiceProvider.GetRequiredService<DatabaseChangelog>();
+        db.Database.Migrate();
+    } catch (Exception ex) {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    } 
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
